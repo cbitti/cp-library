1d99e4b571a1497908d4aca26a19bf053e313350
Christopher Bitti <christopherbitti9@gmail.com>
2025-12-30 15:48:44 -0500

    Finished sparse table

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -1,49 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using ll  = long long;
-using pii = pair<int,int>;
+// Make sure op(x, x) = x for all elements x of type T
+// Make sure op is associative
+// Common examples of op inclue min, max, GCD, bitwise AND/OR
 
-#define all(v) begin(v), end(v)
-
-// Make sure Op(x, x) = x for all elements x of type T; common examples of Op inclue min, max, GCD, bitwise AND/OR
-// Make sure Op is associative
-template<typename T, typename Op>
+template<typename T, typename F>
 struct SparseTable {
 private:
-    vector<T> table;
-    vector<int> lg2;
-    Op op;
+    vector<vector<T>> table;
     int n;
+    F op;
 public:
-    SparseTable(const vector<T>& a) {
-        op = Op();
+    SparseTable(const vector<T>& a, F f) : op(f) {
         int d = 1;
         n = (int)a.size();
-        int k = 0;
-        while (d <= n) {
-            ++k;
-            d <<= 1;
-        }
-        table.assign((ll)n * k, T{});
-        for (int i = 0; i < n; ++i) table[i] = a[i];
+        int k = 32 - __builtin_clz(n);
+        table.assign(k, vector<T>(n));
+        for (int i = 0; i < n; ++i) table[0][i] = a[i];
         for (int p = 1; p < k; ++p) {
             int u = n - (1 << p) + 1;
-            for (int i = 0; i < u; ++i) table[p*n + i] = op(table[(p - 1)*n + i], table[(p - 1)*n + (i + (1 << (p - 1)))]);
-        }
-        lg2.assign(n + 1, 0);
-        int p = 0;
-        d = 1;
-        for (int i = 1; i <= n; ++i) {
-            while (d <= i) {
-                ++p;
-                d <<= 1;
-            }
-            lg2[i] = p - 1;
+            for (int i = 0; i < u; ++i) table[p][i] = op(table[p - 1][i], table[p - 1][i + (1 << (p - 1))]);
         }
     }
+    // 0-indexed, inclusive [l, r]
     T query(int l, int r) const {
-        int p = lg2[r - l + 1];
-        return op(table[p*n + l], table[p*n + (r - (1 << p) + 1)]);
+        int p = 31 - __builtin_clz(r - l + 1);
+        return op(table[p][l], table[p][(r - (1 << p) + 1)]);
     }
 };
